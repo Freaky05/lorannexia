cf1352a6503719e80e9e080defae23a0402cbf12
T0N1 <t0n1@users.noreply.github.com>
2018-05-31 10:53:55 +0200

    Add Walls class

---
+++ src/main/java/view/Walls.java
@@ -0,0 +1,21 @@
+package view;
+
+import java.awt.Rectangle;
+/**
+ * |######################|
+ * | define the hitbox     |
+ * | @return Box          |
+ * |######################|
+ */
+
+public class Walls {
+
+    public int x,y;
+
+    public int getX() {
+        return x;
+    }
+    public int getY() {
+        return y;
+    }
+}
